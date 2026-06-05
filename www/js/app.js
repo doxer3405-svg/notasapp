98be156a1d74e8bb24be06f761fa74e510215459
doxer3405-svg <doxer3405@gmail.com>
2026-06-05 12:38:32 -0500

    Nueva interfaz ImageVault

--- www/js/app.js
+++ www/js/app.js
@@ -46,4 +46,110 @@ function guardarMensaje(){
     };
 
     lector.readAsDataURL(file);
+
+async function guardarMensaje(){
+
+    const archivo =
+    document.getElementById("imagen").files[0];
+
+    if(!archivo){
+        alert("Selecciona una imagen");
+        return;
+    }
+
+    const mensaje =
+    document.getElementById("mensaje").value;
+
+    const lector =
+    new FileReader();
+
+    lector.onload = function(e){
+
+        const img = new Image();
+
+        img.onload = function(){
+
+            const canvas =
+            document.getElementById("canvas");
+
+            const ctx =
+            canvas.getContext("2d");
+
+            canvas.width =
+            img.width;
+
+            canvas.height =
+            img.height;
+
+            ctx.drawImage(img,0,0);
+
+            const datos =
+            ctx.getImageData(
+                0,
+                0,
+                canvas.width,
+                canvas.height
+            );
+
+            const texto =
+            mensaje + "###END###";
+
+            let indice = 0;
+
+            for(
+                let i = 0;
+                i < datos.data.length;
+                i += 4
+            ){
+
+                if(
+                    indice >= texto.length
+                ){
+                    break;
+                }
+
+                datos.data[i] =
+                texto.charCodeAt(
+                    indice
+                );
+
+                indice++;
+            }
+
+            ctx.putImageData(
+                datos,
+                0,
+                0
+            );
+
+            const url =
+            canvas.toDataURL(
+                "image/png"
+            );
+
+            const enlace =
+            document.getElementById(
+                "descarga"
+            );
+
+            enlace.href = url;
+            enlace.download =
+            "imagevault.png";
+
+            enlace.style.display =
+            "block";
+
+            enlace.innerText =
+            "Guardar imagen";
+
+        };
+
+        img.src =
+        e.target.result;
+
+    };
+
+    lector.readAsDataURL(
+        archivo
+    );
 }
